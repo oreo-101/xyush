c917cb021422657165e328c654a71469a3085651
Leo MSI WSL <leo211liao@gmail.com>
2022-08-21 15:22:30 -0700

    move server

--- frontend/vue/src/api/AxiosUtil.ts
+++ frontend/vue/src/api/AxiosUtil.ts
@@ -1,7 +1,11 @@
 
 import axios from 'axios'
 
-const baseURL = process.env.NODE_ENV !== "production" ? "http://localhost:8080" : "???";
+const DEV_SERVER = "http://localhost:8080";
+const DEMO_SERVER = "http://97.120.189.106:8080";
+// override
+const USE_DEV = true;
+const baseURL = process.env.NODE_ENV !== "production" && USE_DEV ? DEV_SERVER : DEMO_SERVER;
 const ax = axios.create({ baseURL });
 
 export default ax;
