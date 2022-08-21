3d25a9ff8c6f3771ed2334ebded162c6e74b94cd
Leo MSI WSL <leo211liao@gmail.com>
2022-08-21 15:01:26 -0700

    add setup data

--- server/src/main/resources/data.sql
+++ server/src/main/resources/data.sql
@@ -1,2 +1,7 @@
-INSERT INTO app_users (username, password, display_name) VALUES
-    ('user1', 'password', 'user1');
+INSERT INTO users (username, password) VALUES
+    ('user1', 'user1');
+
+INSERT INTO roles (name) VALUES ('ROLE_USER');
+INSERT INTO roles (name) VALUES ('ROLE_MODERATOR');
+INSERT INTO roles (name) VALUES ('ROLE_ADMIN');
+--  招新 广场 热门 精选 扩列 二手 游戏
