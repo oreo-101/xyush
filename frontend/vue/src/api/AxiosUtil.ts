b7ce1506afe2fb1dc996b8f91322c94025889998
Leo MSI WSL <leo211liao@gmail.com>
2022-08-21 16:47:38 -0700

    use tls in FE

--- frontend/vue/src/api/AxiosUtil.ts
+++ frontend/vue/src/api/AxiosUtil.ts
@@ -1,8 +1,8 @@
 
 import axios from 'axios'
 
-const DEV_SERVER = "http://localhost:8080";
-const DEMO_SERVER = "http://97.120.189.106:8080";
+const DEV_SERVER = "https://localhost:8080";
+const DEMO_SERVER = "https://97.120.189.106:8080";
 // override
 const USE_DEV = true;
 const baseURL = process.env.NODE_ENV !== "production" && USE_DEV ? DEV_SERVER : DEMO_SERVER;
